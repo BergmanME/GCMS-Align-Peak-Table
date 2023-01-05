af8aced202978c1e042f7d1c782426cde7dffb84
BergmanME <122043211+BergmanME@users.noreply.github.com>
2023-01-05 14:02:37 -0500

    added hello.py (main script) and myname.py module
    For testing

---
+++ Aligned Peak Table.py
@@ -0,0 +1,11 @@
+"""Script for aligning peak tables of GCMS data based on a predefined profile"""
+
+def import_global_packages():
+    """Imports packages that are needed globally"""
+    global math, os, sys, np, pd
+    import math, os, sys
+    import numpy as np
+    import pandas as pd
+    return print("packages imported")
+import_global_packages()
+
